07e8c17827a373fe40ef29ebb36bcf8153c95d49
Andrew <andrewmarkoski@gmail.com>
2020-11-17 16:13:12 -0500

    Added bootstrap alerts for successful user actions

--- mqp_project/users/views.py
+++ mqp_project/users/views.py
@@ -2,17 +2,16 @@ from django.shortcuts import render, redirect
 from .forms import UserRegisterForm, UserUpdateForm
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
-
+# Register new user
 def register(request):
 
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            # username = form.cleaned_data.get('username')
-            # messages.success(request, f'Your account has been created! You are now able to log in')
+            messages.success(request, "Your account has been registered! Please log in.")
             return redirect('users-login')
     else:
 
@@ -23,6 +22,7 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
+# View or update profile page
 @login_required
 def profile(request):
     if request.method == 'POST':
@@ -30,6 +30,7 @@ def profile(request):
 
         if form.is_valid():
             form.save()
+            messages.success(request, "Your account has been updated successfully!")
             return redirect('users-profile')
 
     else:
@@ -42,15 +43,18 @@ def profile(request):
 
     return render(request, 'users/profile.html', context)
 
+# Page for deleting user. Deletes the user when the request is POST
 @login_required
 def delete(request):
     if request.method == 'POST':
         user = request.user
         user.delete()
+        messages.success(request, "Your account was successfully deleted!")
         return redirect('users-login')
 
     return render(request, 'users/delete.html')
 
+# Redirect to login page and show success message after password reset
 def password_reset_complete(request):
-    # Show message that reset was successful
+    messages.success(request, "Your password was reset successfully! Please log in with your new password.")
     return redirect('users-login')
