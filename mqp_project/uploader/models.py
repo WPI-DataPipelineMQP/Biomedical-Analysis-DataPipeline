c28abf876c5d924c7d65a54fa17600fc0873374f
Andrew <andrewmarkoski@gmail.com>
2021-02-28 23:28:48 -0500

    Added documentation for models

--- mqp_project/uploader/models.py
+++ mqp_project/uploader/models.py
@@ -3,9 +3,10 @@ from datapipeline.models import DataCategory
 
 # Create your models here.
 
+# Table to link the name of an uploaded file to its corresponding data category, only used in upload process
 class Document(models.Model):
-    filename = models.CharField(max_length=250)
-    data_category = models.ForeignKey(DataCategory, on_delete=models.CASCADE)
+    filename = models.CharField(max_length=250)                                 # Name of file uploaded
+    data_category = models.ForeignKey(DataCategory, on_delete=models.CASCADE)   # Foreign key to data category that file data will be contained in
     
     class Meta:
         db_table = "document"
